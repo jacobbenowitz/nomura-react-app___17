5eb742e028d9c349be067489d54926fa02561c86
Jacob Benowitz <jacob.benowitz@gmail.com>
2022-07-27 13:17:46 -0400

    setting state is asynchronous

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [a, setA] = useState(0)
+
+  useEffect(() => {
+    console.log('initial value', a)
+    setA(100)
+    console.log('after invoking setA func', a)
+  }, [a])
   return (
     <div className="App">
+      <div>
+        <h2>Value of state variable 'a'</h2>
+        <span>{a}</span>
+      </div>
     </div>
   );
 }
